19373b220496d0fca5e4acaeb36ff710857395fa
goranesine <goranesine@gmail.com>
2019-12-20 23:20:13 +0100

    splashScreen remake

--- lib/ui_pages/splash_screen.dart
+++ lib/ui_pages/splash_screen.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:playground_app/router/router.dart';
 
@@ -7,91 +6,68 @@ class SplashScreen extends StatefulWidget {
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  bool _increaseOpacity = true;
-  Widget _currentWidget;
-  double _opacity = 0;
-  Timer _timer;
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+  String _fadingString;
+  List<String> _strings = [
+    "Mokuteki.io",
+    "Dart Playgrounds",
+    null,
+  ];
 
   void newRouting() {
     Navigator.pushReplacementNamed(context, RouteName.homePage);
   }
 
-  changeWidget() {
-    _currentWidget.toString() == _EmptyWidget().toString()
-        ? _currentWidget = _TextOne()
-        : _currentWidget.toString() == _TextOne().toString()
-            ? _currentWidget = _TextTwo()
-            : _currentWidget.toString() == _TextTwo().toString()
-                ? newRouting()
-                : Container();
+  void changeString() {
+    setState(() {
+      _strings.removeAt(0);
+      _fadingString = _strings.first;
+      _controller.forward();
+    });
   }
 
   @override
   void initState() {
-    _currentWidget = _EmptyWidget();
-    _timer = Timer.periodic(Duration(milliseconds: 200), (timer) {
-      setState(() {
-        if (_increaseOpacity == true) {
-          _opacity += 0.1;
-        }
-        if (_increaseOpacity == false) {
-          _opacity -= 0.1;
-        }
-        if (_opacity > 0.9) {
-          _increaseOpacity = false;
-        }
-        if (_opacity < 0.1) {
-          _increaseOpacity = true;
-          changeWidget();
-        }
-      });
+    _fadingString = _strings.first;
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    _animation = Tween(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(_controller);
+    _controller.forward();
+    _animation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse().whenComplete(changeString);
+      } else if (_fadingString == null) {
+        _controller.stop();
+        newRouting();
+      }
     });
-
     super.initState();
   }
 
   @override
   void dispose() {
-    _timer.cancel();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Opacity(
-        opacity: _opacity,
-        child: _currentWidget,
-      )),
-    );
-  }
-}
-
-class _EmptyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
-
-class _TextOne extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "Mokuteki.io",
-      style: Theme.of(context).textTheme.display1,
-    );
-  }
-}
-
-class _TextTwo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      "Dart Playgrounds",
-      style: Theme.of(context).textTheme.display1,
-    );
+        body: FadeTransition(
+          opacity: _animation,
+          child: Center(
+            child: Text(
+              "$_fadingString",
+              style: TextStyle(color: Colors.black),
+            ),
+          ),
+        ));
   }
 }

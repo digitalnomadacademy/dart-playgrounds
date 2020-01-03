23b18c48b87542fec6ed47fdf861e4ac619498ec
goranesine <goranesine@gmail.com>
2020-01-03 21:56:41 +0100

    removed firebase_core build problems

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:playground_app/provider/provider_main.dart';
 import 'package:playground_app/router/router.dart';
 import 'package:provider/provider.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {

ae7fa70b396019ede7ec1037763defd6644217a7
goranesine <42481446+goranesine@users.noreply.github.com>
2020-01-09 22:05:13 +0100

    ==

--- lib/mvos/model/observable/user_observable.dart
+++ lib/mvos/model/observable/user_observable.dart
@@ -26,4 +26,21 @@ class LoginO {
   const LoginO({@required this.login, @required this.confirmEmailCode})
       : assert(login != null),
         assert(confirmEmailCode != null);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(
+          this, other) ||
+          other is LoginO &&
+              runtimeType == other.runtimeType &&
+              login == other.login &&
+              confirmEmailCode == other.confirmEmailCode;
+
+  @override
+  int get hashCode =>
+      login.hashCode ^
+      confirmEmailCode.hashCode;
+  
+
+
 }

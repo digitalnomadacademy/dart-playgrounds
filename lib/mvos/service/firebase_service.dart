c06d3a216a32f7a035bae7a6d9c67ff8d55b79a9
goranesine <42481446+goranesine@users.noreply.github.com>
2020-01-14 17:23:11 +0100

    added create account and added users collection on firebase

--- lib/mvos/service/firebase_service.dart
+++ lib/mvos/service/firebase_service.dart
@@ -1,12 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:playground_app/logger/logger.dart';
 import 'package:playground_app/mvos/model/entity/user_entity.dart';
 import 'package:playground_app/shared/interfaces.dart';
 import 'package:rxdart/rxdart.dart';
 
 class FirebaseService implements Disposable {
+  final database = Firestore.instance;
+
   PublishSubject<FirebaseUserE> userE$ = PublishSubject<FirebaseUserE>()
-    ..add(FirebaseUserE(uid: null));
+    ..add(FirebaseUserE(uid: null, email: null));
 
   FirebaseService() {
     _initFirebase();
@@ -25,7 +28,15 @@ class FirebaseService implements Disposable {
 
   Future<void> createAccount(String name, String surname, String email,
       String password, String phone, String courseCode) async {
-
+    logger.info("Create account called");
+    await database.collection("users").add({
+      "name": name,
+      "surname": surname,
+      "email": email,
+      "password": password,
+      "phone": phone,
+      "coursecode": courseCode,
+    });
   }
 
   @override

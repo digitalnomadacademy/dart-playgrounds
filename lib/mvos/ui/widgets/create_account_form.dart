83c316d850d84a9cc348c58e82affc3c2274ab01
Robert Kovacic <>
2020-01-14 12:46:08 +0100

    added validators to all form fields

--- lib/mvos/ui/widgets/create_account_form.dart
+++ lib/mvos/ui/widgets/create_account_form.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:playground_app/mvos/model/observable/user_observable.dart';
+import 'package:provider/provider.dart';
 
 class CreateAccountForm extends StatefulWidget {
-
   @override
   _CreateAccountFormState createState() => _CreateAccountFormState();
 }
 
 class _CreateAccountFormState extends State<CreateAccountForm> {
   final formKey = GlobalKey<FormState>();
-  String email;
-  String name;
-  String surname;
-  String phone;
 
+  var _nameController = TextEditingController();
+  var _surnameController = TextEditingController();
+  var _phoneController = TextEditingController();
+  var _emailController = TextEditingController();
+  var _passwordController = TextEditingController();
+  var _repeatPasswordController = TextEditingController();
+  var _codeController = TextEditingController();
+
+  var _surnameFocusNode = FocusNode();
+  var _phoneFocusNode = FocusNode();
+  var _emailFocusNode = FocusNode();
+  var _passwordFocusNode = FocusNode();
+  var _repeatPasswordFocusNode = FocusNode();
+  var _codeFocusNode = FocusNode();
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -23,128 +34,209 @@ class _CreateAccountFormState extends State<CreateAccountForm> {
         child: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              SizedBox(
-                height: 24,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.length < 3) {
-                    return 'Please enter more text';
-                  }
-                  return null;
-                },
-                onSaved: (value)  => name = value,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  labelText: 'Name',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5.0),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.length < 3) {
-                    return 'Please enter more text';
-                  }
-                  return null;
-                },
-                onSaved: (value)  => surname = value,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  labelText: 'Surname',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5.0),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              TextFormField(
-                validator: (val) => !EmailValidator.validate(val, true)
-                    ? 'Not a valid email.'
-                    : null,
-                onSaved: (val) => email = val,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.email),
-                  labelText: 'Email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5.0),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              TextFormField(
-                validator: (value) {
-                  var potentialNumber = int.tryParse(value);
-                  if (potentialNumber == null) {
-                    return 'Enter a phone number';
-                  }
-                },
-                onSaved: (value)  => phone = value,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  icon: Icon(Icons.phone),
-                  labelText: 'Telephone',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5.0),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.vpn_key),
-                  labelText: 'Code(optional)',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5.0),
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Container(
-                width: 180,
-                height: 50,
-                child: RaisedButton(
-                  color: Colors.pink,
-                  //this will be block function instead of arrow,
-                  // because you will first validate form
-                  // before sending the data trough observable.
-                  // only if observable future gets resolved,
-                  // you will push new route
-                  onPressed: () {
-                    if (formKey.currentState.validate()) {
-                      formKey.currentState.save();
-                      print(name);
-                      print(surname);
-                      print(email);
-                      print(phone);
-                    }
-                  },
 
-                  child: Text(
-                    'Create account',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+              SizedBox(
+                height: 20,
+              ),
+              Container(height: 50,
+                child: TextFormField(
+                  controller: _nameController,
+                  textInputAction: TextInputAction.go,
+                  onEditingComplete: () => _surnameFocusNode.requestFocus(),
+                  validator: (value) {
+                    if (value.length < 3) {
+                      return 'Please enter more text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.person),
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(height: 50,
+                child: TextFormField(
+                  focusNode: _surnameFocusNode,
+                  textInputAction: TextInputAction.go,
+                  onEditingComplete: () => _phoneFocusNode.requestFocus(),
+                  controller: _surnameController,
+                  validator: (value) {
+                    if (value.length < 3) {
+                      return 'Please enter more text';
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.person),
+                    labelText: 'Surname',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),Container(height: 50,
+                child: TextFormField(
+                  focusNode: _phoneFocusNode,
+                  textInputAction: TextInputAction.go,
+                  onEditingComplete: () => _emailFocusNode.requestFocus(),
+                  controller: _phoneController,
+                  validator: (value) {
+                    if (value.length<9) {
+                      return 'Please enter more number';
+
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.phone),
+                    labelText: 'Telephone',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(height: 50,
+                child: TextFormField(
+                  focusNode: _emailFocusNode,
+                  controller: _emailController,
+                  textInputAction: TextInputAction.go,
+                  onEditingComplete: () => _passwordFocusNode.requestFocus(),
+                  validator: (val) => !EmailValidator.validate(val, true)
+                      ? 'Not a valid email.'
+                      : null,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.email),
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              SizedBox(
+                height: 20,
+              ),
+              Container(height: 50,
+                child: TextFormField(
+                  focusNode: _passwordFocusNode,
+                  textInputAction: TextInputAction.go,
+                  onEditingComplete: () => _repeatPasswordFocusNode.requestFocus(),
+                  controller: _passwordController,
+                  validator: (value) {
+                   if (value.length < 6) {
+                     return 'Please enter more text';
+                   }  return null;
+                  },
+                  keyboardType: TextInputType.phone,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.keyboard),
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(height: 50,
+                child: TextFormField(
+                  focusNode: _repeatPasswordFocusNode,
+                  textInputAction: TextInputAction.go,
+                  onEditingComplete: () => _codeFocusNode.requestFocus(),
+                  controller: _repeatPasswordController,
+                  validator: (value) {
+                  if (value != _passwordController.text) {
+                    return 'Password is not matching';
+                  }return null;
+                  },
+                  keyboardType: TextInputType.phone,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.keyboard),
+                    labelText: 'RepeatPassword',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(height: 50,
+                child: TextFormField(
+                  focusNode: _codeFocusNode,
+                  controller: _codeController,
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.vpn_key),
+                    labelText: 'Code(optional)',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Provider<CreateAccountO>.value(
+                // todo delete once goran creates observable
+                value: CreateAccountO(),
+                child: Consumer<CreateAccountO>(
+                  builder: (context, o, _) => Container(
+                    width: 180,
+                    height: 50,
+                    child: RaisedButton(
+                      color: Colors.pink,
+                      //this will be block function instead of arrow,
+                      // because you will first validate form
+                      // before sending the data trough observable.
+                      // only if observable future gets resolved,
+                      // you will push new route
+                      onPressed: () {
+                        if (formKey.currentState.validate()) {
+//                        call o.createAccount(name:_controller.name,....
+                        }
+                      },
+
+                      child: Text(
+                        'Create account',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
                   ),
                 ),
               )
@@ -155,3 +247,9 @@ class _CreateAccountFormState extends State<CreateAccountForm> {
     );
   }
 }
+
+class CreateAccountO {
+  //todo delete
+}
+
+

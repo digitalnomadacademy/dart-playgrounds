617ebc72a36ac47ad78ae837443c028ccf744121
lea <lea.barasin@gmail.com>
2020-01-26 17:43:15 +0100

    changed arguments in if

--- lib/mvos/ui/widgets/drawer.dart
+++ lib/mvos/ui/widgets/drawer.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:playground_app/mvos/model/observable/user_observable.dart';
-import 'package:playground_app/mvos/model/user_model.dart';
 import 'package:playground_app/router/router.dart';
 import 'package:provider/provider.dart';
 
@@ -15,60 +14,60 @@ class CustomDrawer extends StatelessWidget {
     return Container(
       color: Colors.white,
       width: 200,
-      child: Column(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(28.0),
-            child: FlatButton(
-              child: Text(
-                'About',
-                style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+      child: Consumer<LoggedInO>(builder:(context,data2,_)=>Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: FlatButton(
+                child: Text(
+                  'About',
+                  style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                ),
+                onPressed: () {
+                  route != RouteName.aboutPage
+                      ? Navigator.pushNamed(context, RouteName.aboutPage)
+                      : Navigator.pop(context);
+                },
               ),
-              onPressed: () {
-                route != RouteName.aboutPage
-                    ? Navigator.pushNamed(context, RouteName.aboutPage)
-                    : Navigator.pop(context);
-              },
             ),
-          ),
-          Divider(
-            color: Colors.pink,
-            thickness: 2,
-          ),
-          SizedBox(height: 400),
-          Consumer<LogOutA>(
-            builder: (context, o, _) => FlatButton(
-              onPressed: () async {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Consumer<LoggedInO>(
-                        builder: (context, data2, _) => Center(
-                          child: CircularProgressIndicator(
-                            backgroundColor: Colors.white,
-                          ),
-                        ),
-                      );
+            Divider(
+              color: Colors.pink,
+              thickness: 2,
+            ),
+            SizedBox(height: 400),
+            Consumer<LogOutA>(
+              builder: (context, o, _) => FlatButton(
+                onPressed: () async {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return Center(
+                            child: CircularProgressIndicator(
+                              backgroundColor: Colors.pink,
+                            ),
+                          );
+                      });
+                  if (data2.loggedIn == true) {
+                    o.logOut().then((_) {
+                      Navigator.pushReplacementNamed(
+                          context, RouteName.logInPage);
+                      print('ruta');
+                    }).catchError((e) {
+                      Navigator.pop(context);
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text(e.toString()),
+                      ));
                     });
-                if (Login == Login) {
-                  o.logOut().then((_) {
-                    Navigator.pushReplacementNamed(
-                        context, RouteName.logInPage);
-                  }).catchError((e) {
-                    Navigator.pop(context);
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text(e.toString()),
-                    ));
-                  });
-                }
-              },
-              child: Text(
-                'Logout',
-                style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                  }
+                },
+                child: Text(
+                  'Logout',
+                  style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

4505b3a80ede3db414594bc7435144a988d18749
Matej The Tree <matejthetree@gmail.com>
2020-01-27 21:33:14 +0100

    base za tvoj branch

--- lib/mvos/model/entity/courses_entity.dart
+++ lib/mvos/model/entity/courses_entity.dart
@@ -33,66 +33,18 @@ class CoursesE {
 }
 
 class CourseE {
-  final String courseID;
-
+  final String id;
   final String name;
   final String description;
-
-  final double progress;
-
   final Color color;
+  final String videoPlaylistUrl;
+  List<LessonE> lessons;
+}
 
-  final bool unlocked;
-
-  const CourseE({
-    @required this.courseID,
-    @required this.name,
-    @required this.color,
-    @required this.description,
-    @required this.progress,
-    @required this.unlocked,
-  }) : assert(progress <= 1 && progress >= 0, 'Progress out of bounds');
-
-  @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is CourseE &&
-          runtimeType == other.runtimeType &&
-          courseID == other.courseID &&
-          name == other.name &&
-          description == other.description &&
-          progress == other.progress &&
-          color == other.color &&
-          unlocked == other.unlocked;
-
-  @override
-  int get hashCode =>
-      courseID.hashCode ^
-      name.hashCode ^
-      description.hashCode ^
-      progress.hashCode ^
-      color.hashCode ^
-      unlocked.hashCode;
-
-  Map<String, dynamic> toMap() {
-    return {
-      'courseID': this.courseID,
-      'name': this.name,
-      'description': this.description,
-      'progress': this.progress,
-      'color': this.color,
-      'unlocked': this.unlocked,
-    };
-  }
-
-  factory CourseE.fromMap(Map<String, dynamic> map) {
-    return new CourseE(
-      courseID: map['courseID'] as String,
-      name: map['name'] as String,
-      description: map['description'] as String,
-      progress: map['progress'] as double,
-      color: map['color'] as Color,
-      unlocked: map['unlocked'] as bool,
-    );
-  }
+class LessonE {
+  final String id;
+  final String name;
+  final String description;
+  final String videoUrl;
+  final String githubUrl;
 }

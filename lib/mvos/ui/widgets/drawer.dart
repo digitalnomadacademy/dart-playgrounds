8278ecd3a7058b413c50e1ab6ae0eaec8e9bf325
lea <lea.barasin@gmail.com>
2020-01-27 21:03:22 +0100

    applied changes to logout button

--- lib/mvos/ui/widgets/drawer.dart
+++ lib/mvos/ui/widgets/drawer.dart
@@ -3,6 +3,8 @@ import 'package:playground_app/mvos/model/observable/user_observable.dart';
 import 'package:playground_app/router/router.dart';
 import 'package:provider/provider.dart';
 
+import 'logout_button.dart';
+
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({
     Key key,
@@ -14,7 +16,8 @@ class CustomDrawer extends StatelessWidget {
     return Container(
       color: Colors.white,
       width: 200,
-      child: Consumer<LoggedInO>(builder:(context,data2,_)=>Column(
+      child: Consumer<LoggedInO>(
+        builder: (context, oLoggedIn, _) => Column(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(28.0),
@@ -34,38 +37,7 @@ class CustomDrawer extends StatelessWidget {
               color: Colors.pink,
               thickness: 2,
             ),
-            SizedBox(height: 400),
-            Consumer<LogOutA>(
-              builder: (context, o, _) => FlatButton(
-                onPressed: () async {
-                  showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return Center(
-                            child: CircularProgressIndicator(
-                              backgroundColor: Colors.pink,
-                            ),
-                          );
-                      });
-                  if (data2.loggedIn == true) {
-                    o.logOut().then((_) {
-                      Navigator.pushReplacementNamed(
-                          context, RouteName.logInPage);
-                      print('ruta');
-                    }).catchError((e) {
-                      Navigator.pop(context);
-                      Scaffold.of(context).showSnackBar(SnackBar(
-                        content: Text(e.toString()),
-                      ));
-                    });
-                  }
-                },
-                child: Text(
-                  'Logout',
-                  style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
+            oLoggedIn.loggedIn ? LogOutButton() : Container(),
           ],
         ),
       ),

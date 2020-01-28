4e35cd45472c1cebb5671baabc01b43bd2c994e5
Robert Kovacic <>
2020-01-28 21:27:56 +0100

    after error keyboard is no longer visible

--- lib/mvos/ui/widgets/loginPage_form.dart
+++ lib/mvos/ui/widgets/loginPage_form.dart
@@ -86,6 +86,7 @@ class _LoginFormState extends State<LoginForm> {
                     height: 50,
                     child: RaisedButton(
                       onPressed: () async {
+                        FocusScope.of(context).requestFocus(FocusNode());
 //                    show spinning circle dialog, while we wait for the future
                         showDialog(
                             context: context,

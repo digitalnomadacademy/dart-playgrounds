6e2f7a99c4a142d29c8717687754ffeeb8d6dc8b
lea <lea.barasin@gmail.com>
2020-02-03 22:16:07 +0100

    added push notification option.

---
+++ lib/mvos/ui/widgets/push_notifications.dart
@@ -0,0 +1,161 @@
+import 'dart:async';
+
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class PushNotification extends StatefulWidget {
+  @override
+  _PushNotificationState createState() => _PushNotificationState();
+}
+
+class _PushNotificationState extends State<PushNotification> {
+  String _stringText = 'Waiting for token...';
+  bool _newNotification = true;
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print('onMessage: $message');
+        setState(() {
+          _newNotification;
+        });
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+        _navigateToItemDetail(message);
+      },
+    );
+
+    //Needed by iOS only
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+
+    //Getting the token from FCM
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {
+        _stringText = "Push Messaging token: \n\n $token";
+      });
+      print(_stringText);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Notifications'),
+        ),
+        body: Material(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+              child: Text(
+                _stringText,
+                style: TextStyle(fontSize: 19),
+              ),
+            ),
+          ),
+        ));
+  }
+
+  //PRIVATE METHOD TO HANDLE NAVIGATION TO SPECIFIC PAGE
+  void _navigateToItemDetail(Map<String, dynamic> message) {
+    final MessageBean item = _itemForMessage(message);
+    // Clear away dialogs
+    Navigator.popUntil(context, (Route<dynamic> route) => route is PageRoute);
+    if (!item.route.isCurrent) {
+      Navigator.push(context, item.route);
+    }
+  }
+}
+
+final Map<String, MessageBean> _items = <String, MessageBean>{};
+MessageBean _itemForMessage(Map<String, dynamic> message) {
+  //If the message['data'] is non-null, we will return its value, else return map message object
+  final dynamic data = message['data'] ?? message;
+  final String itemId = data['id'];
+  final MessageBean item = _items.putIfAbsent(
+      itemId, () => MessageBean(itemId: itemId))
+    ..status = data['status'];
+  return item;
+}
+
+//Model class to represent the message return by FCM
+class MessageBean {
+  MessageBean({this.itemId});
+  final String itemId;
+
+  StreamController<MessageBean> _controller =
+      StreamController<MessageBean>.broadcast();
+  Stream<MessageBean> get onChanged => _controller.stream;
+
+  String _status;
+  String get status => _status;
+  set status(String value) {
+    _status = value;
+    _controller.add(this);
+  }
+
+  static final Map<String, Route<void>> routes = <String, Route<void>>{};
+  Route<void> get route {
+    final String routeName = '/detail/$itemId';
+    return routes.putIfAbsent(
+      routeName,
+      () => MaterialPageRoute<void>(
+        settings: RouteSettings(name: routeName),
+        builder: (BuildContext context) => DetailPage(itemId),
+      ),
+    );
+  }
+}
+
+//Detail UI screen that will display the content of the message return from FCM
+class DetailPage extends StatefulWidget {
+  DetailPage(this.itemId);
+  final String itemId;
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  MessageBean _item;
+  StreamSubscription<MessageBean> _subscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _item = _items[widget.itemId];
+    _subscription = _item.onChanged.listen((MessageBean item) {
+      if (!mounted) {
+        _subscription.cancel();
+      } else {
+        setState(() {
+          _item = item;
+        });
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Item ${_item.itemId}"),
+      ),
+      body: Material(
+        child: Center(child: Text("Item status: ${_item.status}")),
+      ),
+    );
+  }
+}

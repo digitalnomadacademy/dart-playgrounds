7c88bc86af5cb94198bfa12aaa511ba61c349a74
SempaiEcchi <minecraftm665@gmail.com>
2020-02-10 00:10:59 +0100

    no message

--- lib/mvos/ui/widgets/edit_course_form.dart
+++ lib/mvos/ui/widgets/edit_course_form.dart
@@ -8,10 +8,30 @@ class EditCourseForm extends StatefulWidget {
 }
 
 class _EditCourseFormState extends State<EditCourseForm> {
+  TextEditingController _courseNameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<ActiveCourseO>(builder: (context, activeCourseO, child) {
-      return Text(activeCourseO.activeCourse.name);
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 75,
+          child: TextFormField(
+            controller: _courseNameController
+              ..text = (activeCourseO.activeCourse.name),
+            decoration: InputDecoration(
+              icon: Icon(Icons.text_fields),
+              labelText: 'Name',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5.0),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
     });
   }
 }

907e1837f4b464d328a0713237dea34ebe20b38c
SempaiEcchi <minecraftm665@gmail.com>
2020-02-09 20:46:38 +0100

    Closes #51

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:playground_app/config/app_config.dart';
 import 'package:playground_app/provider/provider_main.dart';
 import 'package:playground_app/router/router.dart';
 import 'package:provider/provider.dart';
@@ -27,7 +28,9 @@ class MyApp extends StatelessWidget {
           ),
         ),
         onGenerateRoute: Router.generator,
-        initialRoute: RouteName.homePage,
+        initialRoute: kAppEnvironment == AppEnvironment.develop
+            ? RouteName.homePage
+            : RouteName.splashScreen,
       ),
     );
   }

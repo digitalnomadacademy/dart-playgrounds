72eb5ac78bdce353610a481e1213ee53654084ef
SempaiEcchi <minecraftm665@gmail.com>
2020-02-10 01:28:37 +0100

    no message

--- lib/mvos/ui/widgets/edit_course_form.dart
+++ lib/mvos/ui/widgets/edit_course_form.dart
@@ -24,94 +24,96 @@ class _EditCourseFormState extends State<EditCourseForm> {
     return Consumer<ActiveCourseO>(builder: (context, activeCourseO, child) {
       return Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 75,
-              child: TextFormField(
-                controller: _courseNameController
-                  ..text = (activeCourseO.activeCourse.name),
-                decoration: InputDecoration(
-                  icon: Icon(Icons.text_fields),
-                  labelText: 'Name',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 75,
+                child: TextFormField(
+                  controller: _courseNameController
+                    ..text = (activeCourseO.activeCourse.name),
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.text_fields),
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: <Widget>[
-                Flexible(
-                  child: Container(
-                    height: 75,
-                    child: TextFormField(
-                      controller: _courseDescriptionController
-                        ..text = (activeCourseO.activeCourse.description),
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.text_fields),
-                        labelText: 'Description',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(5.0),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      height: 75,
+                      child: TextFormField(
+                        controller: _courseDescriptionController
+                          ..text = (activeCourseO.activeCourse.description),
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.text_fields),
+                          labelText: 'Description',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5.0),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.attachment),
-                  tooltip: "From TXT",
-                  onPressed: () {
+                  IconButton(
+                    icon: Icon(Icons.attachment),
+                    tooltip: "From TXT",
+                    onPressed: () {
 //                    _showDialog(context);
-                  },
-                )
-              ],
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              height: 75,
-              child: TextFormField(
-                controller: _coursePlaylistURLController
-                  ..text = (activeCourseO.activeCourse.videoPlaylistUrl),
-                decoration: InputDecoration(
-                  icon: Icon(Icons.video_library),
-                  labelText: 'Video playlist URL',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(5.0),
+                    },
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                height: 75,
+                child: TextFormField(
+                  controller: _coursePlaylistURLController
+                    ..text = (activeCourseO.activeCourse.videoPlaylistUrl),
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.video_library),
+                    labelText: 'Video playlist URL',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(5.0),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            RaisedButton(
-              onPressed: () {
+              SizedBox(
+                height: 30,
+              ),
+              RaisedButton(
+                onPressed: () {
 //                showColorPicker();
-              },
-              child: Icon(Icons.colorize),
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            RaisedButton(
-              child: Icon(Icons.mode_edit),
-              onPressed: () {
-                courseService.updateCourseData(initialName);
-              },
-            )
-          ],
+                },
+                child: Icon(Icons.colorize),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              RaisedButton(
+                child: Icon(Icons.mode_edit),
+                onPressed: () {
+                  courseService.updateCourseData(initialName);
+                },
+              )
+            ],
+          ),
         ),
       );
     });

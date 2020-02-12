bd722f56033bfe78eb51e5b6cf3b3c3955ac32e1
Matej The Tree <matejthetree@gmail.com>
2020-02-12 20:34:21 +0100

    welcome page as default login

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         ),
         onGenerateRoute: Router.generator,
         initialRoute: kAppEnvironment == AppEnvironment.develop
-            ? RouteName.logInPage
+            ? RouteName.welcomePage
             : RouteName.splashScreen,
       ),
     );

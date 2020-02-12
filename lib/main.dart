c9be767f4a76e3e65796775f1fa75da989579e63
Matej The Tree <matejthetree@gmail.com>
2020-02-12 20:33:43 +0100

    login page as default development screen

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         ),
         onGenerateRoute: Router.generator,
         initialRoute: kAppEnvironment == AppEnvironment.develop
-            ? RouteName.homePage
+            ? RouteName.logInPage
             : RouteName.splashScreen,
       ),
     );
